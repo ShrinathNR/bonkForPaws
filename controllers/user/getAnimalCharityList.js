4ea80149f7808f8c07ccace6b6f9d00f3dbcb810
ShrinathNR <nr.shrinath@gmail.com>
2024-01-18 14:45:27 +0530

    add db query to getAnimalCharity

--- controllers/user/getAnimalCharityList.js
+++ controllers/user/getAnimalCharityList.js
@@ -1,12 +1,32 @@
 import data from "../../output.json" with {type:"json"};
+import sqlite3 from "sqlite3";
+sqlite3.verbose();
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename);
+const dbDirectory = __dirname+'./../../db/bonk.db';
 
 const getAnimalCharityList = async (req, res) => {
     try {
-        res.status(200).json({
-            code: 200,
-            error: false,
-            charity: data
+        let db = new sqlite3.Database(dbDirectory, sqlite3.OPEN_READWRITE, (err)=> {
+            if(err) throw new Error(err.message);
+            console.log("connection successful");
+        });
+        
+        db.all('SELECT * FROM charity', (err, rows) => {
+            if(err) throw new Error(err.message);
+            res.status(200).json({
+                code: 200,
+                error: false,
+                charity: rows
+            })
         })
+
+        db.close((err)=>{
+            if(err) return console.log(err);
+        })
+        
     } catch (error) {
         console.log(error);
         res.status(400).json({
